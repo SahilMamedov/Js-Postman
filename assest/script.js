e650f1d66f3d055d19a1cfba8966eeed5e7c035e
SahilMamedov <sahilim@code.edu.az>
2022-06-02 05:05:41 +0400

    finish js code

--- assest/script.js
+++ assest/script.js
@@ -8,6 +8,37 @@
   let notFound=document.getElementById("notFound")
   let select =document.getElementById("select")
 
+  let weatherCondition=document.getElementById("weatherCondition")
+
+    search.addEventListener("click",function(ev){
+    ev.preventDefault()
+    
+    axios.get(`https://api.weatherapi.com/v1/current.json?key=6bc15cfb31414fbda9f95625221905&q=${input.value}`)
+    .then(function (res) {
+        
+    city.innerText=`City: ${res.data.location.name}`
+    country.innerText=`Country: ${res.data.location.country}`
+    weather.innerText=`Weather Forecats: ${res.data.current.temp_c}`
+    condition.innerText=`Sky Condition:`
+    img.src=`https:${res.data.current.condition.icon}`
+    weatherCondition.innerText=res.data.current.condition.text
+
+        if(select.value=="fahrenheit"){
+            weather.innerText=`Weather Forecats: ${res.data.current.temp_f}`
+            
+        }
+    
+
+   
+    
+    })
+    .catch(function (error) {
+        notFound.innerText="Not Found..!!!"
+    
+    })
+
+
+})
 
 
   
