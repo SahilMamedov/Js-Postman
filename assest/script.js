606307a808fa1ef569895c3a4dc334055a8ee2eb
SahilMamedov <sahilim@code.edu.az>
2022-06-02 05:04:41 +0400

    Feat: get element by id

--- assest/script.js
+++ assest/script.js
@@ -1,4 +1,12 @@
-
+  let city=document.getElementById("city")
+  let country=document.getElementById("country")
+  let weather=document.getElementById("weather")
+  let condition=document.getElementById("condition")
+  let search=document.getElementById("search")
+  let input=document.getElementById("input")
+  let img=document.getElementById("img")
+  let notFound=document.getElementById("notFound")
+  let select =document.getElementById("select")
 
 
 
